6a611dc05b0c9a4f2e78623c3f0be3552d445c58
Cloynn_ <notcloynn@gmail.com>
2022-10-06 14:12:59 +0700

    Add MediaQuery

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+  function scrollDown() {
+    window.scrollBy(0, 800);
+    behavior: 'smooth'
+  }
+
+  const mediaQuery = window.matchMedia('(min-width: 768px)')
+
+  if (mediaQuery.matches) {
+  function scrollDown() {
+    window.scrollBy(0, 50);
+    behavior: 'smooth'
+  }
+}
